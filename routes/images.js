0425b5227d69f5a6094802c9b29029dee5c53cb7
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2021-04-23 17:55:52 -0300

    create files

--- routes/images.js
+++ routes/images.js
@@ -1,19 +1,29 @@
 const {Router} = require('express');
 const router = Router();
-
-
+const multer = require('multer')
+const fs = require("fs");
+const { promisify } = require("util");
+const pipeline = promisify(require("stream").pipeline);
 const Tesseract = require('tesseract.js');
 
 
 
-
-    
-
+const upload= multer()
 router.route('/')
-    .post(async (req,res)=>{
-        const tes = await Tesseract.recognize('./image.jpg','spa')
+    .post(upload.single('file'),async (req,res)=>{
+        //console.log(req.file.buffer.toString("base64"))
+        
+        console.log(req.file.originalname)
+        console.log(req.file)
+        await require("fs").writeFile(req.file.originalname, req.file.buffer.toString('base64'), 'base64', function(err) {
+            console.log(err);
+          });
 
-        res.send(tes.data.text)
+        //if (file.detectedFileExtension != ".jpg") 
+
+        const tes = await Tesseract.recognize(req.file.originalname,'spa')
+        
+         res.send(tes.data.text) 
     })
 
 module.exports = router;
